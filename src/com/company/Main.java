7ce619590355aa0a8b5b34337298a6cec21428dd
class2-2 <drfsvgkf@gmail.ru>
2021-10-09 13:04:59 +0300

    изменён код и добавлен метод Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    Test t1 = new Test();
+    t1.setI(2);
+
+    Test t2=t1;
+
+    System.out.println(t2.getI());
+
+    t2.setI(5);
+
+    System.out.println(t1.getI());
+
     }
 }
---
+++ src/com/company/Test.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Test {
+    int i;
+
+    public int getI() {
+        return i;
+    }
+    public void setI(int i){
+        this.i=i;
+    }
+}
